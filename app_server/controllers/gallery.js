617a0bca26ad223aa6235a145fa6d1fd2bd8c006
ke1r0 <whtrfck@gmail.com>
2016-08-14 12:57:59 +0300

    services list moved to controllers

--- app_server/controllers/gallery.js
+++ app_server/controllers/gallery.js
@@ -1,6 +1,9 @@
 /* GET gallery page */
 module.exports.mainPage = function(req, res, next) {
-	res.render('gallery', { title: 'Художник Анна Королюк'});
+	res.render('gallery', {
+    title: 'Художник Анна Королюк: Галерея',
+    description: 'Примеры творческих работ'
+  });
 };
 /* GET gallery list */
 module.exports.list = function(req, res, next) {
